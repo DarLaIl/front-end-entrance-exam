2a3e1f437cde85e190ca364117a45aed396fda2f
DarLaIl <darlail.work.mail@gmail.com>
2024-08-03 20:57:49 +0300

    Добавила кнопки для изменения и сохранения резюме

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-import '../css/style.css'
+import '../css/style.css';
+
+const changeResume = document.querySelector('#changeResume');
+const saveChenges = document.querySelector('#saveChenges');
+const downloadResume = document.querySelector('#downloadResume');
+const updateBtn = document.querySelectorAll('.updateBtn');
+
+changeResume.addEventListener('click', () => {
+    updateBtn.forEach((btn) => {
+        btn.classList.replace('disabled','activeBtn')
+    });
+});
